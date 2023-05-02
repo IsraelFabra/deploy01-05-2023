6d5c102d739e814afb928f07b5010d0f34617443
IsraelFabra <israelfabra@gmail.com>
2023-05-02 03:05:36 +0200

    1

--- index.js
+++ index.js
@@ -1,55 +1,30 @@
 /* console.clear();
 
-console.log("Hola mundo9");
+console.log("EYYYYYY");
+
+import {createServer} from 'http';
+
+const httpServer = createServer((req, res) => {
+    console.log("PETICION RECIBIDA");
+
+    res.end("RECIBIDO COLEGA")
+});
+
+httpServer.listen(3000);
  */
 
+console.clear();
 
-// importar el modulo de "express" de la carpeta "node_modules"
-const express = require('express');
-// Creamos un objeto "app" a traves del modulo (clase) de express
-const app = new express();
-// puerto por el que el servidor local escucha. "http://localhost:3000"
-const port = 3000;
+import {createServer} from 'http';
 
-// MIDDLEWARES
-// middleware que gestiona los mensajes JSON
-app.use(express.json());
-// middleware que configura el directorio de los archivos estáticos (img, css, js....)
-app.use(express.static('public'));
+const httpServer = createServer((req, res) => {
+    console.log(req.method);
 
-// rutas: Se establecen las rutas configuradas para este server, mediante los métodos GET, POST, PUT, DELETE.
-// Estos métodos gestionan las peticiones (req) y respuestas (res) mediante un callback
-// Ruta 1- En este caso devuelve al cliente un texto plano "Hello World!"
-app.get('/hello_world', (req, res) => {
-  res.send('Hello World !')
+    console.log(req.url);
+
+    console.log(req.headers);
+
+    
+
+    res.end("RECIBIDO COLEGA")
 });
-// Ruta 2- En este caso devuelve al cliente un archivo html
-// __dirname: ruta absoluta del server donde se alberga el proyecto
-// ESTA RUTA LA UTILIZAMOS PARA EL ENVÍO DEL FORM AL CLIENTE
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-});
-
-// RUTA QUE RECOGE EL DATO DEL FORM DEL CLIENTE
-app.post('/dato', (req, res) => {
-  const dato = req.body;
-  console.log(dato);
-  res.json("Dato recibido ok!!!");
-});
-
-
-// Crea el webserver y escucha por el puerto 3000, devolviendo un mensaje por consola
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
-
-
-
-
-
-
-
-
-
-
-
